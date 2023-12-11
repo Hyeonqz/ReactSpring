21d3a554e22e94dfb6f6497dc2080c93dea71eb3
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2023-12-11 12:40:42 +0900

    #4 react

--- reactwork/react-start/src/index.js
+++ reactwork/react-start/src/index.js
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import FirstApp from './components/FirstApp';
-import ThirdApp from './components/ThirdApp';
-import FouthApp from './components/FouthApp';
-import FifthApp from './components/FifthApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-     {/* <App />
-    <FirstApp/> 
-    <secondsApp/> */}
-    {/* <ThirdApp/>
-    <FouthApp/> */}
-    <FifthApp/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
