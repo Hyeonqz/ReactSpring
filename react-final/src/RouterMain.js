7490fddac57d87d0d62eb2ac8eecbe54a3d211dc
진현규 <108057548+jinhyeonkyu@users.noreply.github.com>
2023-12-19 12:37:30 +0900

    TODO : "react&spring#2"

--- react-final/src/RouterMain.js
+++ react-final/src/RouterMain.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import {About, Info, Menu, Title} from "./components";
-import {Route, Routes} from "react-router-dom";
-import Shop from "./shop/Shop";
-import Board from "./board/Board";
-import Member from "./member/Member";
-import Login from "./login/Login";
-import {Main} from "./components";
-import {ShopDetail, ShopForm} from "./shop";
+import { Info, Title, Menu, Main, About } from './components';
+import { Routes, Route } from 'react-router-dom';
+import Shop from './shop/Shop';
+import Board from './board/Board';
+import Member from './member/Member';
+import Login from './login/Login';
+import { ShopDetail, ShopForm } from './shop';
 
-function RouterMain(props) {
+function RouteMain(props) {
     return (
         <div>
             <div className='title'><Title/></div>
-            <div className='menu'><Menu/></div>
             <div className='info'><Info/></div>
+            <div className='menu'><Menu/></div>
             <div className='main'>
                 <Routes>
                     <Route path='/' element={<Main/>}/>
-                    {/*SHop*/}
+
+
+
+                    {/* shop */}
                     <Route path='/shop/list' element={<Shop/>}/>
                     <Route path='/shop/form' element={<ShopForm/>}/>
+                    {/* :num은 num값을 넘기는 것
+                        backend에서 먼저 num값 넘겨주는 것 처리해줘야함 */}
                     <Route path='/shop/detail/:num' element={<ShopDetail/>}/>
 
                     <Route path='/board/list' element={<Board/>}/>
@@ -28,13 +32,14 @@ function RouterMain(props) {
                     <Route path='/about' element={<About/>}/>
                     <Route path='*' element={
                         <div>
-                            <h1>잘못된 주소 입니다.</h1>
+                            <h1>잘못된 주소입니다</h1>
                         </div>
                     }/>
+
                 </Routes>
             </div>
         </div>
     );
 }
 
-export default RouterMain;
+export default RouteMain;
--- react-final/src/shop/ShopDetail.js
+++ react-final/src/shop/ShopDetail.js
@@ -1,31 +1,165 @@
-import React, {useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
-import axios from "axios";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+
+const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>,
+) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
 
 function ShopDetail(props) {
-    const {num} = useParams();
-    const[data,setData] = useState('');
 
+    //넘겨준 num값을 읽어야한다
+    const { num } = useParams();
+
+    //dto가 들어올 변수 선언
+    const [data, setData] = useState('');
+
+    //image 출력용 url / detail url
+    let detailUrl = "http://localhost:9000/shop/detail?num=" + num;
     let photoUrl = "http://localhost:9000/save/";
-    let detailUrl = "http://localhost:9000/shop/detail?num"+num;
+    let deleteUrl = "http://localhost:9000/shop/delete?num=" + num;
 
-    //Spring으로 부터 num에 해당하는 data받기
-    const onDataReceived =()=> {
+    //spring으로부터 num에 해당하는 data 받기
+    const onDataReceive = () => {
+
         axios.get(detailUrl)
-            .then(res=>{
+            .then(res => {
+
+                console.log(res.data.sangpum); //sangpum값만 가져와서 확인
+                //spring에서 넘긴 data를 여기서 변경해줌
                 setData(res.data);
+            }).catch(err => {
+            alert(err.data);
+        })
+    }
+
+    //처음 렌더링시 위의 함수 호출
+    useEffect(() => {
+        onDataReceive();
+    }, []);
+
+    const navi = useNavigate();
+
+    //삭제 다이얼로그 코드추가(mui)
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    //삭제시 호출될 함수
+    const onDelete = () => {
+        axios.delete(deleteUrl)
+            .then(res => {
+                // 성공적인 삭제
+                console.log(res.data);
+                // 삭제 후 이동
+                navi("/shop/list");
+            })
+            .catch(error => {
+                // 자세한 오류 정보를 콘솔에 로그
+                console.error("Delete Error:", error);
+                // 필요한 대로 오류 처리
+            })
+            .finally(() => {
+                // 성공 또는 실패 여부에 관계없이 다이얼로그 닫기
+                handleClose();
             });
     }
 
-    //처음 랜더링시 위의 함수 호출
-    useEffect(()=>{
-        onDataReceived();
-    },[]);
+
+
+
+
+
 
     return (
         <div>
-            <h2>{num}</h2>
-            <h1>ShopDetail이다</h1>
+            {/* num 출력확인시 db에서 가져올 수 있음 */}
+            {/*<h1>{num}</h1>*/}
+            <table className='table table-bordered' style={{ width: '700px', marginLeft: '100px' }}>
+                <caption align='top'><b>상품상세보기</b></caption>
+                <tbody>
+                <tr style={{ height: '150px' }}>
+                    <td width='350' rowSpan='4'>
+                        <img src={photoUrl + data.photo} className='large' />
+                    </td>
+                    <td>
+                        <h4>상품명: {data.sangpum}</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>수량: {data.su}</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>단가: {data.dan}</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h4>입고일: {data.ipgoday}</h4>
+                    </td>
+                </tr>
+                <tr>
+                    <td colSpan='2' align='center'>
+                        <button type='button' className='btn btn-outline-success'
+                                onClick={() => {
+                                    navi('/shop/list');
+                                }}>목록</button>
+                        <button type='button' className='btn btn-outline-info'
+                                onClick={() => {
+                                    navi('/shop/form');
+                                }}>추가</button>
+                        <button type='button' className='btn btn-outline-warning'>수정</button>
+                        <button type='button' className='btn btn-outline-danger' onClick={handleClickOpen}>삭제</button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
+
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>
+                    <h4>삭제하고 싶니??</h4>
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        <h5>삭제하고 싶으면 [Delete] 눌러라</h5>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button onClick={onDelete}>Delete</Button>
+                </DialogActions>
+            </Dialog>
+
         </div>
     );
 }
